6d9d89f4a54ddcf88733d104a280c7327cdf8f5d
Yashasvi1202 <vanani.yashu25@gmail.com>
2026-03-11 20:26:33 -1000

    check empty dict.py

---
+++ check empty dict.py
@@ -0,0 +1,11 @@
+d1={1:10,2:20}
+d2={}
+if len(d1)==0:
+        print("Dictionary is empty")
+else:
+        print("Dictionary is not empty")
+
+if len(d2)==0:
+        print("Dictionary is empty")
+else:
+        print("Dictionary is not empty")        
