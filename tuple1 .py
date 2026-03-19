62865f9fc040545a5b92d53ee6a0ab52ecd29a82
Yashasvi1202 <vanani.yashu25@gmail.com>
2026-03-18 21:06:53 -1000

    Create tuple1 .py

---
+++ tuple1 .py
@@ -0,0 +1,7 @@
+my_tuple = ("apple", "banana", "cherry", "date")
+temp_list = list(my_tuple)
+temp_list.remove("banana")
+newtuple = tuple(templist)
+
+print(f"Original: {mytuple}")
+print(f"Updated:  {newtuple}")
